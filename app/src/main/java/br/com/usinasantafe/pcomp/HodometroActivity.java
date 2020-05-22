d04d37fd575fce3b60a5aa5e6af1b8e2de6a886f
andinhodelgado <anderson.sdelgado@gmail.com>
2020-05-22 12:30:09 -0300

    Refatoramento de código II - Versão 3.00 QA - Chamado 16975

---
+++ app/src/main/java/br/com/usinasantafe/pcomp/HodometroActivity.java
@@ -0,0 +1,175 @@
+package br.com.usinasantafe.pcomp;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import br.com.usinasantafe.pcomp.control.CheckListCTR;
+import br.com.usinasantafe.pcomp.util.ConexaoWeb;
+
+public class HodometroActivity extends ActivityGeneric {
+
+    private PCOMPContext pcompContext;
+    private Double horimetroNum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hodometro);
+
+        pcompContext = (PCOMPContext) getApplication();
+
+        Button buttonOkHorimetro = (Button) findViewById(R.id.buttonOkPadrao);
+        Button buttonCancHorimetro = (Button) findViewById(R.id.buttonCancPadrao);
+
+        TextView textViewHorimetro = (TextView) findViewById(R.id.textViewPadrao);
+        if (pcompContext.getVerPosTela() == 1) {
+            textViewHorimetro.setText("HODOMETRO INICIAL");
+        }
+        else if (pcompContext.getVerPosTela() == 8) {
+            textViewHorimetro.setText("HODOMETRO FINAL");
+        }
+        else if (pcompContext.getVerPosTela() == 9) {
+            textViewHorimetro.setText("HODOMETRO FINAL");
+        }
+        else if (pcompContext.getVerPosTela() == 10) {
+            textViewHorimetro.setText("HODOMETRO INICIAL");
+        }
+
+        buttonOkHorimetro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!editTextPadrao.getText().toString().equals("")) {
+
+                    String horimetro = editTextPadrao.getText().toString();
+                    horimetroNum = Double.valueOf(horimetro.replace(",", "."));
+
+                    if (horimetroNum >= pcompContext.getConfigCTR().getConfig().getHorimetroConfig()) {
+                        verTela();
+                    } else {
+
+                        AlertDialog.Builder alerta = new AlertDialog.Builder(HodometroActivity.this);
+                        alerta.setTitle("ATENÇÃO");
+                        alerta.setMessage("O HODOMETRO DIGITADO " + horimetroNum + " É MENOR QUE O HODOMETRO ANTERIOR DA MAQUINA " + pcompContext.getConfigCTR().getConfig().getHorimetroConfig() + ". DESEJA MANTER ESSE VALOR?");
+
+                        alerta.setPositiveButton("SIM", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                verTela();
+                            }
+
+                        });
+
+                        alerta.setNegativeButton("NÃO", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+
+                        alerta.show();
+
+                    }
+
+                }
+
+            }
+        });
+
+        buttonCancHorimetro.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if (editTextPadrao.getText().toString().length() > 0) {
+                    editTextPadrao.setText(editTextPadrao.getText().toString().substring(0, editTextPadrao.getText().toString().length() - 1));
+                }
+            }
+        });
+
+    }
+
+    public void verTela(){
+        if (pcompContext.getVerPosTela() == 1) {
+            salvarBoletimAberto();
+        }
+        else if (pcompContext.getVerPosTela() == 8) {
+            salvarBoletimFechado();
+        }
+    }
+
+    public void salvarBoletimAberto() {
+
+        pcompContext.getConfigCTR().setHorimetroConfig(horimetroNum);
+
+        pcompContext.getMotoMecCTR().setHodometroInicialBol(horimetroNum, getLongitude(), getLatitude());
+        pcompContext.getMotoMecCTR().salvarBolAbertoMM();
+
+        CheckListCTR checkListCTR = new CheckListCTR();
+        if(checkListCTR.verAberturaCheckList(pcompContext.getMotoMecCTR().getTurno())){
+            Long statusCon;
+            ConexaoWeb conexaoWeb = new ConexaoWeb();
+            if (conexaoWeb.verificaConexao(HodometroActivity.this)) {
+                statusCon = 1L;
+            }
+            else{
+                statusCon = 0L;
+            }
+            pcompContext.getMotoMecCTR().insParadaCheckList(getLongitude(), getLatitude(), statusCon);
+            pcompContext.setPosCheckList(1);
+            checkListCTR.createCabecAberto(pcompContext.getMotoMecCTR());
+            if (pcompContext.getVerAtualCL().equals("N_AC")) {
+                Intent it = new Intent(HodometroActivity.this, PergAtualCheckListActivity.class);
+                startActivity(it);
+                finish();
+            } else {
+                Intent it = new Intent(HodometroActivity.this, ItemCheckListActivity.class);
+                startActivity(it);
+                finish();
+            }
+        }
+        else{
+            Intent it = new Intent(HodometroActivity.this, MenuMotoMecActivity.class);
+            startActivity(it);
+            finish();
+        }
+    }
+
+    public void salvarBoletimFechado() {
+
+        pcompContext.getConfigCTR().setHorimetroConfig(horimetroNum);
+        pcompContext.getMotoMecCTR().setHodometroFinalBol(horimetroNum);
+        pcompContext.getMotoMecCTR().salvarBolFechadoMM();
+
+        if (pcompContext.getVerPosTela() == 8) {
+            Intent it = new Intent(HodometroActivity.this, MenuInicialActivity.class);
+            startActivity(it);
+            finish();
+        } else if (pcompContext.getVerPosTela() == 9) {
+            pcompContext.setVerPosTela(10);
+            Intent it = new Intent(HodometroActivity.this, FuncionarioActivity.class);
+            startActivity(it);
+            finish();
+        }
+
+    }
+
+    public void onBackPressed() {
+        if (pcompContext.getVerPosTela() == 1) {
+            Intent it = new Intent(HodometroActivity.this, ListaAtividadeActivity.class);
+            startActivity(it);
+            finish();
+        }
+        else {
+            Intent it = new Intent(HodometroActivity.this, MenuMotoMecActivity.class);
+            startActivity(it);
+            finish();
+        }
+    }
+
+}
--- app/src/main/java/br/com/usinasantafe/pcomp/ListaAtividadeActivity.java
+++ app/src/main/java/br/com/usinasantafe/pcomp/ListaAtividadeActivity.java
@@ -1,13 +1,152 @@
 package br.com.usinasantafe.pcomp;
 
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class ListaAtividadeActivity extends Activity {
+import java.util.ArrayList;
+
+import br.com.usinasantafe.pcomp.model.bean.estaticas.AtividadeBean;
+import br.com.usinasantafe.pcomp.util.ConexaoWeb;
+
+public class ListaAtividadeActivity extends ActivityGeneric {
+
+    private ListView ativListView;
+    private PCOMPContext pcompContext;
+    private ProgressDialog progressBar;
+    private ArrayList ativArrayList;
+    private Long nroOS = 0L;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_atividade);
+
+        pcompContext = (PCOMPContext) getApplication();
+
+        Button buttonAtualAtividade = (Button) findViewById(R.id.buttonAtualAtividade);
+        Button buttonRetAtividade = (Button) findViewById(R.id.buttonRetAtividade);
+        TextView textViewTituloAtividade = (TextView) findViewById(R.id.textViewTituloAtividade);
+
+        nroOS =  pcompContext.getConfigCTR().getConfig().getOsConfig();
+
+        buttonAtualAtividade.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                ConexaoWeb conexaoWeb = new ConexaoWeb();
+
+                if (conexaoWeb.verificaConexao(ListaAtividadeActivity.this)) {
+
+                    progressBar = new ProgressDialog(v.getContext());
+                    progressBar.setCancelable(true);
+                    progressBar.setMessage("Atualizando Atividades...");
+                    progressBar.show();
+
+                    pcompContext.getMotoMecCTR().verAtiv( String.valueOf(nroOS), ListaAtividadeActivity.this, ListaAtividadeActivity.class, progressBar);
+
+                } else {
+
+                    AlertDialog.Builder alerta = new AlertDialog.Builder(ListaAtividadeActivity.this);
+                    alerta.setTitle("ATENÇÃO");
+                    alerta.setMessage("FALHA NA CONEXÃO DE DADOS. O CELULAR ESTA SEM SINAL. POR FAVOR, TENTE NOVAMENTE QUANDO O CELULAR ESTIVE COM SINAL.");
+                    alerta.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+
+                    alerta.show();
+
+                }
+
+            }
+        });
+
+        buttonRetAtividade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(ListaAtividadeActivity.this, OSActivity.class);
+                startActivity(it);
+            }
+        });
+
+        if(pcompContext.getVerPosTela() == 1){
+            textViewTituloAtividade.setText("ATIVIDADE PRINCIPAL");
+        } else {
+            textViewTituloAtividade.setText("ATIVIDADE");
+        }
+
+        ativArrayList = pcompContext.getMotoMecCTR().getAtivArrayList(nroOS);
+
+        ArrayList<String> itens = new ArrayList<String>();
+        for (int i = 0; i < ativArrayList.size(); i++) {
+            AtividadeBean atividadeBean = (AtividadeBean) ativArrayList.get(i);
+            itens.add(atividadeBean.getCodAtiv() + " - " + atividadeBean.getDescrAtiv());
+        }
+
+        AdapterList adapterList = new AdapterList(this, itens);
+        ativListView = (ListView) findViewById(R.id.listAtividade);
+        ativListView.setAdapter(adapterList);
+
+        ativListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> l, View v, int position,
+                                    long id) {
+                // TODO Auto-generated method stub
+
+                AtividadeBean atividadeBean = (AtividadeBean) ativArrayList.get(position);
+                ativArrayList.clear();
+
+                pcompContext.getConfigCTR().setAtivConfig(atividadeBean.getIdAtiv());
+
+                if(pcompContext.getVerPosTela() == 2){
+
+                    Long statusCon;
+                    ConexaoWeb conexaoWeb = new ConexaoWeb();
+                    if (conexaoWeb.verificaConexao(ListaAtividadeActivity.this)) {
+                        statusCon = 1L;
+                    }
+                    else{
+                        statusCon = 0L;
+                    }
+
+                    pcompContext.getMotoMecCTR().setAtivApont(atividadeBean.getIdAtiv());
+                    pcompContext.getMotoMecCTR().insApontMM(getLongitude(), getLatitude(), statusCon);
+
+                    Intent it = new Intent(ListaAtividadeActivity.this, MenuMotoMecActivity.class);
+                    startActivity(it);
+                    finish();
+
+                }
+                else{
+
+                    pcompContext.getMotoMecCTR().setOSBol(pcompContext.getConfigCTR().getConfig().getOsConfig());
+                    pcompContext.getMotoMecCTR().setAtivBol(atividadeBean.getIdAtiv());
+
+                    Intent it = new Intent(ListaAtividadeActivity.this, HodometroActivity.class);
+                    startActivity(it);
+                    finish();
+
+                }
+
+            }
+
+        });
+
     }
+
+    public void onBackPressed() {
+    }
+
 }
